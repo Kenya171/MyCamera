b208f5368de13a7a2b8bff23146db1a709775b9d
Kenya171 <fujikawa.k569@gmail.com>
2023-07-06 17:33:42 +0900

    カメラアプリ作成

--- MyCamera/ContentView.swift
+++ MyCamera/ContentView.swift
@@ -6,18 +6,106 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    //撮影した写真を保存する状態変数
+    @State var captureImage:UIImage? = nil
+    //撮影画面（sheet)の開閉状態を管理
+    @State var isShowSheet = false
+    //フォトライブラリーで選択した写真を管理
+    @State var photoPickerSelectedImage: PhotosPickerItem? = nil
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+            //スペース追加
+            Spacer()
+            //撮影した写真がある時
+            if let captureImage{
+                //撮影写真を表示
+                Image(uiImage: captureImage)
+                //リサイズ
+                    .resizable()
+                //アスペクト比(縦横比)を維持して画面に収める
+                    .scaledToFit()
+            }
+            Spacer()
+            //「カメラを起動する」ボタン
+            Button{
+                //カメラが利用可能かチェック
+                if UIImagePickerController.isSourceTypeAvailable(.camera){
+                    print("カメラは使用できます")
+                    //カメラが使えるなら、isShowSheetをtrue
+                    isShowSheet.toggle()
+                }else{
+                    print("カメラは使用できません")
+                }
+                //ボタンをタップした時のアクション
+            }label: {
+                Text("カメラを起動する")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                //文字列をセンタリング指定
+                    .multilineTextAlignment(.center)
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+            }//カメラを起動するボタンここまで
+            //上下左右に余白を追加
+            .padding()
+            //sheetを表示
+            //isPresentedで指定した状態変数がtrueの時実行
+            .sheet(isPresented: $isShowSheet){
+                //UIImagePickerCntroller(写真撮影）を表示
+                ImagePickerView(isShowSheet: $isShowSheet, captureImage: $captureImage)
+            }
+            
+            //フォトライブラリーから選択する
+            PhotosPicker(selection: $photoPickerSelectedImage, matching: .images,preferredItemEncoding: .automatic, photoLibrary:.shared()){
+                Text("フォトライブラリーから選択する")
+                    .frame(maxWidth:.infinity)
+                    .frame(height: 50)
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .padding()
+            }//PhotosPickerここまで
+            //選択した写真をもとに写真を取り出す
+            .onChange(of: photoPickerSelectedImage){ photosPickerItem in
+                //選択した写真がある時
+                if let photosPickerItem{
+                    //Data型で写真を取り出す
+                    photosPickerItem.loadTransferable(type: Data.self) { result in switch result{
+                    case.success(let data):
+                        //写真がある時
+                        if let data{
+                            //写真をcaptureImageに保存
+                            captureImage = UIImage(data: data)
+                        }
+                    case.failure:
+                        return
+                    }
+                        
+                    }
+                }
+            }//onChangeここまで
+            //captureImageをアンラップする
+            if let captureImage{
+               //captureImageから共有する画面を生成する
+               let shareImage = Image(uiImage: captureImage)
+                //共有シート
+                ShareLink(item: shareImage, subject: nil, message: nil,
+                          preview: SharePreview("photo",image: shareImage)) {
+                    Text("SNSに投稿する")
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 50)
+                        .background(Color.blue)
+                        .foregroundColor(Color.white)
+                        .padding()
+                } //shareLinkここまで
+            }//アンラップここまで
+            
+        }//VStackここまで
+    }//boddyここまで
+}//ContentViewここまで
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- MyCamera/ImagePickerView.swift
+++ MyCamera/ImagePickerView.swift
@@ -2,38 +2,65 @@
 //  ImagePickerView.swift
 //  MyCamera
 //
-//  Created by 藤川賢也 on 2023/05/01.
-//
+//  Created by 賢也
 
 import SwiftUI
 
 struct ImagePickerView: UIViewControllerRepresentable {
-    //UIImagePickerController(写真撮影)が表示されているかを管理
+    // UIImagePickerController(写真撮影)が表示されているかを管理
     @Binding var isShowSheet: Bool
-    //撮影した写真を格納する変数
+    // 撮影した写真を格納する変数
     @Binding var captureImage: UIImage?
     
-    //Coordinatorでコントローラーのdelegateを管理
-    class Coordinator: NSObject, UINavigationControllerDelegate,UIImagePickerControllerDelegate{
-        //ImagePickerView型の定数を用意
+    // Coordinatorでコントローラのdelegateを管理
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        // ImagePickerView型の定数を用意
         let parent: ImagePickerView
         
-        //イニシャライザ
-        init(parent: ImagePickerView) {
+        // イニシャライザ
+        init(_ parent: ImagePickerView) {
             self.parent = parent
         }
         
-        //撮影が終わっとときに呼ばれるdelegateメソッド、必ず必要
+        // 撮影が終わったときに呼ばれるdelegateメソッド、必ず必要
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-            //撮影した写真をcaptureImageに保存
-            if let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            
+            // 撮影した写真をcaptureImageに保存
+            if let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
                 parent.captureImage = originalImage
             }
-            //sheetを閉じる
+            // sheetを閉じる
             parent.isShowSheet.toggle()
         }
-       
-    }//Coorfinatorここまで
-}
-
+        
+        // キャンセルボタンが選択されたときに呼ばれるdelegateメソッド、必ず必要
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            // sheetを閉じる
+            parent.isShowSheet.toggle()
+        }
+    } // Coordinatorここまで
+    
+    // Coordinatorを生成、SwiftUIによって自動的に呼び出し
+    func makeCoordinator() -> Coordinator {
+        // Coordinatorクラスのインスタンスを生成
+        Coordinator(self)
+    } // makeCoordinatorここまで
+    
+    // Viewを生成するときに実行
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        // UIImagePickerControllerのインスタンスを生成
+        let myImagePickerController = UIImagePickerController()
+        // sourceTypeにcameraを設定
+        myImagePickerController.sourceType = .camera
+        // delegate設定
+        myImagePickerController.delegate = context.coordinator
+        // UIImagePickerControllerを返す
+        return myImagePickerController
+    } // makeUIViewControllerここまで
+    
+    // Viewが更新されたときに実行
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        // 処理なし
+    } // updateUIViewControllerここまで
+} // ImagePickerViewここまで
 
